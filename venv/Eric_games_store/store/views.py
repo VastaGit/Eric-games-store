153ac046d0f2021a306ab3b2b20b8ac9fe1c9f00
Vasta_git <askatseitakunov@gmail.com>
2023-04-30 13:32:02 +0300

    playing around with models

--- venv/Eric_games_store/store/views.py
+++ venv/Eric_games_store/store/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Game
+
+# def store(request):
+#   template = loader.get_template('first.html')
+#   return HttpResponse(template.render())
 
 def store(request):
-  template = loader.get_template('first.html')
-  return HttpResponse(template.render())
+  mygames = Game.objects.all().values()
+  template = loader.get_template('all_games.html')
+  context = {
+    'mygames': mygames,
+  }
+  return HttpResponse(template.render(context, request))
